e6008278a034ec6bd36dbcc2612526b3f3976d66
Zoltan Polgar <zpolgar@chemaxon.com>
2016-10-26 12:02:58 +0200

    adjust configuration

--- tracking-client/src/main/java/com/chemaxon/analytics/MQSettings.java
+++ tracking-client/src/main/java/com/chemaxon/analytics/MQSettings.java
@@ -2,7 +2,7 @@ package com.chemaxon.analytics;
 
 public class MQSettings {
     public static final String LOCAL_BROKER_URI = "vm://test?broker.persistent=true";
-    public static final String REMOTE_BROKER_URI = "tcp://localhost:61616";
+    public static final String REMOTE_BROKER_URI = "nio://localhost:61616";
 
     public static final String LOCAL_ERRORS = "LOCAL.ERRORS";
     public static final String LOCAL_USAGE = "LOCAL.USAGE";
